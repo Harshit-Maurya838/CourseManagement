90c6e4a7f2250e1bb6ea82eb89532be644a6d5fb
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2026-06-28 22:13:32 +0530

    feat: create User

--- src/main/java/com/app/coursemanagement/controller/UserController.java
+++ src/main/java/com/app/coursemanagement/controller/UserController.java
@@ -1,16 +1,33 @@
 package com.app.coursemanagement.controller;
 
+import com.app.coursemanagement.common.ApiResponse;
+import com.app.coursemanagement.models.User;
+import com.app.coursemanagement.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> fetchAllUsers(){
+    public ResponseEntity<ApiResponse<User>> createUser(@RequestBody User user){
+        User createdUser = userService.createUser(user);
+        ApiResponse<User> response = new ApiResponse<>(
+                true,
+                "User created successfully",
+                createdUser
+        );
 
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(response);
     }
 }
--- src/main/java/com/app/coursemanagement/service/UserService.java
+++ src/main/java/com/app/coursemanagement/service/UserService.java
@@ -1,5 +1,7 @@
 package com.app.coursemanagement.service;
 
+import com.app.coursemanagement.exception.UserAlreadyExistsException;
+import com.app.coursemanagement.models.User;
 import com.app.coursemanagement.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -7,5 +9,13 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public User createUser(User user) {
+        if(userRepository.existsByEmail(user.getEmail())){
+            throw new UserAlreadyExistsException("Email already exists");
+        }
+
+        return userRepository.save(user);
+    }
 }
